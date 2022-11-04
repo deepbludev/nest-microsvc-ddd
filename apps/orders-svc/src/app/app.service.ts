d7acc1b01d4c132ba3676e99e270ffde3904321d
Carlo Casorzo <carlo@deepblu.dev>
2022-11-04 05:40:39 +0100

    chore: configure nginx in docker

--- apps/orders-svc/src/app/app.service.ts
+++ apps/orders-svc/src/app/app.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: 'Welcome to orders-svc!' };
+    return { message: 'Welcome to orders-svc from Docker!' }
   }
 }
