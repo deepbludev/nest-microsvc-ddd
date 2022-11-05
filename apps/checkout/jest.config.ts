e298f952c486a77c7d4e85aa5056f1050b1e520a
Carlo Casorzo <carlo@deepblu.dev>
2022-11-05 06:34:26 +0100

    chore: change name from orders-svc to checkout

--- apps/checkout/jest.config.ts
+++ apps/checkout/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'orders-svc',
+  displayName: 'checkout',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -12,5 +12,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/orders-svc',
-};
+  coverageDirectory: '../../coverage/apps/checkout',
+}
