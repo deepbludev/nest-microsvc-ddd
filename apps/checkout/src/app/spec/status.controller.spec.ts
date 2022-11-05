9fcdc293fe876626f72b4cf3772b0d4128b4f392
Carlo Casorzo <carlo@deepblu.dev>
2022-11-05 06:39:27 +0100

    feat: add healthcheck endpoint

---
+++ apps/checkout/src/app/spec/status.controller.spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { StatusController } from '../status.controller'
+
+describe('StatusController', () => {
+  let app: TestingModule
+  let statusCtrl: StatusController
+
+  beforeAll(async () => {
+    app = await Test.createTestingModule({
+      controllers: [StatusController],
+      providers: [],
+    }).compile()
+
+    statusCtrl = app.get(StatusController)
+  })
+
+  describe('/status GET', () => {
+    it('should return status OK', () => {
+      expect(statusCtrl.status()).toEqual({
+        message: '[checkout] All systems operational',
+      })
+    })
+  })
+})
